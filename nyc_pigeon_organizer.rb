11aa9879708206c32782ea3d9ec53dacdb64f538
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-07 03:30:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,6 +8,7 @@ def nyc_pigeon_organizer(data)
   name_list << data[:gender][:female]
   name_list.flatten!
   
+<<<<<<< HEAD
   
   while i < name_list.length
     pidgeon_list[name_list[i]] = {}
@@ -19,6 +20,16 @@ def nyc_pigeon_organizer(data)
         puts pidgeon_list
         if pidgeons.include?(name_list[i])
           
+=======
+  while i < name_list.length
+    pidgeon_list[name_list[i]] = {}
+    data.collect do |group, info|
+      
+      pidgeon_list[name_list[i]][group] = []
+      info.collect do |aspect, pidgeons|
+        if pidgeons.include?(name_list[i])
+          binding.pry
+>>>>>>> e73cb8f37ad3bac22a4779528006d15b79540e66
           pidgeon_list[name_list[i]][group] << aspect.to_s
           
         end
@@ -27,5 +38,8 @@ def nyc_pigeon_organizer(data)
     print pidgeon_list
     i+=1
   end
+<<<<<<< HEAD
   return pidgeon_list
+=======
+>>>>>>> e73cb8f37ad3bac22a4779528006d15b79540e66
 end
